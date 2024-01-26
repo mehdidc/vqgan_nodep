6b924c136cee1bad354d72bdf3f2a42dc0b29993
Mehdi Cherti <mehdicherti@gmail.com>
2024-01-26 20:17:26 +0100

    add example with real image / reconstruction

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget https://github.com/mehdidc/feed_forward_vqgan_clip/releases/download/0.1/vqgan_imagenet_f16_16384.yaml
+wget https://github.com/mehdidc/feed_forward_vqgan_clip/releases/download/0.1/vqgan_imagenet_f16_16384.ckpt
--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import torch
 from vqgan import VQModel
 from omegaconf import OmegaConf
+import torchvision
+from PIL import Image
 config = OmegaConf.load("vqgan_imagenet_f16_16384.yaml")
 model = VQModel(**config.model.params)
 model.eval().requires_grad_(False)
 model.init_from_ckpt("vqgan_imagenet_f16_16384.ckpt")
-x = torch.randn(1, 3, 256, 256)
+img = Image.open("dog.jpg")
+img = img.resize((224, 224))
+# to Tensor
+x = torchvision.transforms.ToTensor()(img)
+x = x.unsqueeze(0)
 ids = model.tokenize(x)
 xr = model.reconstruct_from_tokens(ids)
-print(x.shape, xr.shape)
+# to pil
+xr = xr.squeeze(0).permute(1, 2, 0).clamp(0, 1).numpy()
+xr = (xr * 255).astype("uint8")
+xr = Image.fromarray(xr)
+xr.save("dog_recon.jpg")
